c3348d0a0b5c7962307143d1e0d480007582e604
U-LYNDONLAROUCHE\Patrik <Patrik@LyndonLaRouche.(none)>
2014-05-10 10:04:38 +0200

    kjgl

--- testMultiply.java
+++
@@ -1,9 +0,0 @@
-@Test
-public void testMultiply() {
-
-   // MyClass is tested
-   MyClass tester = new MyClass();
-   
-   // Check if multiply(10,5) returns 50
-   assertEquals("Result", 50, tester.multiply(10, 5));
- }
